4e2c6be7a12efaae3633a4d0f307f4e68d996e2d
Ariane Laferriere-Martineau <e1757139@cmaisonneuve.qc.ca>
2021-03-31 09:38:49 -0400

    modif adaptabitlite

--- src/Accueil.jsx
+++ src/Accueil.jsx
@@ -10,10 +10,10 @@ export default function Accueil() {
       <div className="desc">
         <p>
         <span>Qui je suis <Face /> : </span>
-          Je suis une jeune femme passionnée par les jeux vidéos et le Web. Je possède une personnalité autant créative que technique, ce qui fait de moi quelqu'un de très polyvalente qui aime autant la <span>création</span> que la <span>programmation</span> d'un projet multimédia J'ai étudié en programme linguistique au secondaire ce qui fait de moi une personne <span>bilingue</span>. Au cégep, j'ai choisi ma technique avec comme but de travailler un jour dans l'industrie du <span>jeu vidéo</span>. Aujourd'hui, en plus de on intérêt marqué vers cette industrie, j'ai également développé un intérêt comme <span>développeuse Web</span>.
+          Je suis une jeune femme passionnée par les jeux vidéos et le Web. Je possède une personnalité autant créative que technique, ce qui fait de moi quelqu'un de très polyvalente qui aime autant la <span>création</span> que la <span>programmation</span> d'un projet multimédia J'ai étudié en programme linguistique au secondaire ce qui fait de moi une personne <span>bilingue</span>. Au cégep, j'ai choisi ma technique avec comme but de travailler un jour dans l'industrie du <span>jeu vidéo</span>. Aujourd'hui, en plus de mon intérêt marqué vers cette industrie, j'ai également développé un intérêt comme <span>développeuse Web</span>.
         </p>
 
-        <div>
+        <div className="listeAime">
           <span>Ce que j'aime <Favorite /> : </span>
           <ul>
             <li><Games fontSize="small"/> Jeux Vidéo</li>
